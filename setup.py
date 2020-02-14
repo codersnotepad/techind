769af2e10bea55f188f6168c17c39b11e7e92257
timotewb <timote.wb@gmail.com>
2020-02-14 21:16:43 +0000

    Updates for v 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy>=1.18.1"]
 
 setup(
     name="techind",
-    version="0.0.5",
+    version="0.0.6",
     author="Timote WB",
     author_email="timote.wb@gmail.com",
     description="Collection of technical indicators for financial time series analysis.",
